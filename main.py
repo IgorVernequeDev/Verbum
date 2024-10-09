d90572007bd5487ab5ca9522c2594cf884ae6c05
IgorVernequeDev <igorverneque5@gmail.com>
2024-10-08 21:00:03 -0300

    Inserção dos modelos as páginas HTML.

--- main.py
+++ main.py
@@ -14,59 +14,43 @@ db = mysql.connector.connect(
     database="VERBUM"
 )
 
-# @app.route('/adicionar_livro', methods=['GET', 'POST'])
-# def adicionar_livro():
-#     if request.method == 'POST':
-#         titulo = request.form['titulo']
-#         autor = request.form['autor']
-#         editora = request.form['editora']
-#         ano_publicacao = request.form['ano']
-#         quantidade = request.form['quantidade']
-#         categoria = request.form['categoria']
-#         # Lê a imagem como binário
-#         imagem_capa = request.files['imagem'].read()
-
-#         cursor = db.cursor()
-#         sql = """INSERT INTO Livro (idAutor, idEditora, titulo, anoPublicacao, quantidade, categoria, imagemCapa)
-#                  VALUES (%s, %s, %s, %s, %s, %s, %s)"""
-#         valores = (autor, editora, titulo, ano_publicacao,
-#                    quantidade, categoria, imagem_capa)
-#         cursor.execute(sql, valores)
-#         db.commit()
-
-#         return 'Livro adicionado com sucesso!'
-
-#     return render_template('index.html')
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/home')
 def home():
-    logado = session.get('logado', False)
+    logado = session.get('logado', True)
     return render_template('index.html', logado=logado)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/recuperarsenha')
-def recuperarsenha():
-    return render_template('recuperarsenha.html')
-
 @app.route('/livros')
 def livros():
-    return render_template('livros.html')
+    logado = session.get('logado', False) 
+    return render_template('livros.html', logado=logado)
 
 @app.route('/contato')
 def contato():
-    return render_template('contato.html')
+    logado = session.get('logado', False) 
+    return render_template('contato.html', logado=logado)
 
 @app.route('/modelo')
 def modelo():
     logado = session.get('logado', True)
     return render_template('modelo.html', logado=logado)
+
+@app.route('/adm')
+def adm():
+    logado = session.get('logado', True)
+    return render_template('adm_index.html', logado=logado)
+
+@app.route('/cadlivro')
+def cadlivro():
+    logado = session.get('logado', True) 
+    return render_template('cadlivro.html', logado=logado)
 
 @app.route('/login', methods=['POST'])
 def logar():
@@ -74,7 +58,10 @@ def logar():
     senha = request.form['senha']
 
     if email == 'admin@gmail.com' and senha == '123':
-        session['logado'] = True  # Armazena o estado de login na sessão
+        session['logado'] = True
+        return redirect('/adm')
+    elif email == 'aluno@gmail.com' and senha == '123':
+        session['logado'] = True
         return redirect('/home')
     else:
         return render_template("login.html",msg="Usuário/Senha estão incorretos!")
