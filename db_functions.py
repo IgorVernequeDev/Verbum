95f2e25d4c9981566679eebde6ab02756e61cfc1
Igor Verneque <129201436+IgorVernequeDev@users.noreply.github.com>
2024-10-18 15:42:28 -0300

    Melhorias no back-end (cadastro de livro)

--- db_functions.py
+++ db_functions.py
@@ -1,14 +1,15 @@
 import mysql.connector
+from mysql.connector import connect, Error
 from config import *
 
 def conectar_db():
     conexao = mysql.connector.connect(
-        host = DB_HOST,
-        user = DB_USER,
-        password = DB_PASSWORD,
-        database = DB_NAME
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
     )
-    cursor = conexao.cursor()
+    cursor = conexao.cursor(dictionary=True)
     return conexao, cursor
 
 def encerrar_db(cursor, conexao):
